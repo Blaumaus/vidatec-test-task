fdb1dea78331867ba3ad48ca9750f18d9588869a
Blue Mouse <durexp@protonmail.ch>
2023-03-09 03:18:17 +0000

    fixed incorrect item colour

--- src/ui/Select.tsx
+++ src/ui/Select.tsx
@@ -26,7 +26,7 @@ const Select: React.FC<ISelect> = ({
       <>
         <Listbox.Label className='block text-sm whitespace-pre-line font-medium text-gray-100'>{label}</Listbox.Label>
         <div className='mt-1 relative'>
-          <Listbox.Button className='relative w-full bordertext-gray-50 border-gray-800 bg-gray-700 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'>
+          <Listbox.Button className='relative w-full bordertext-gray-50 border-gray-800 bg-gray-700 text-gray-50 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm'>
             <span className='block truncate first-letter:capitalize'>{title}</span>
             <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
               <ChevronUpDownIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
